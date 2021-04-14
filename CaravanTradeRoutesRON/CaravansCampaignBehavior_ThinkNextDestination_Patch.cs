88a77ffa81285797bd98e15d6750a4fb5f70fea1
Ronimjan <ronimjan.programmer@gmail.com>
2021-04-14 13:59:29 +0200

    Add project files.

---
+++ CaravanTradeRoutesRON/CaravansCampaignBehavior_ThinkNextDestination_Patch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
+
+namespace CaravanTradeRoutesRON
+{
+    [HarmonyPatch(typeof(CaravansCampaignBehavior), "ThinkNextDestination")]
+    public class CaravansCampaignBehavior_ThinkNextDestination_Patch
+    {
+        public static void Postfix(ref Town __result, MobileParty caravanParty)
+        { 
+            if(caravanParty.LeaderHero != Hero.MainHero) {  return; }
+
+            Town nextTown = null; 
+            __result = nextTown;
+        }
+    }
+}
---
+++ CaravanTradeRoutesRON/SubModule.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+using System.Xml.Linq;
+using TaleWorlds.Library;
+using System;
+
+namespace CaravanTradeRoutesRON
+{
+    public class SubModule : MBSubModuleBase
+    {
+        public Dictionary<string, List<Town>> tradeRoutes = new Dictionary<string, List<Town>>();
+        public readonly string xmlPath = String.Concat(BasePath.Name, "Modules/tradeRoutes.xml");
+
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            if (!(game.GameType is Campaign)) { return; }
+
+            XDocument doc = XDocument.Load(xmlPath);           
+            XElement tradeRoutes = doc.Root.Element("tradeRoutes");
+            IEnumerable<XElement> listTradeRoutes = tradeRoutes.Elements();
+            IEnumerable<XElement> tempTown;
+            List<Town> tempTownList = new List<Town>();
+
+            foreach(XElement tradeRouteCount in listTradeRoutes)
+            {
+                tempTown = tradeRouteCount.Element("Towns").Elements();
+                foreach(XElement townCount in tempTown)
+                {
+                    tempTownList.Add(Campaign.);
+                }
+            }
+
+            new HarmonyLib.Harmony("WorkshopStashRON.patcher").PatchAll();
+        }
+    }
+}
