c610c8ef498bf9159fa80300683d30202224be21
Ronimjan <ronimjan.programmer@gmail.com>
2021-04-30 00:43:59 +0200

    fixed loading issue 1 - already registered

--- CaravanTradeRoutesRON/SubModule.cs
+++ CaravanTradeRoutesRON/SubModule.cs
@@ -61,6 +61,29 @@ namespace CaravanTradeRoutesRON
                         }
                     }
                 }
+                bool tradeRouteErrorAlreadyRegistered = tradeRoutes.TryGetValue(tradeRouteCount.Attribute("name").Value, out Dictionary<int, Town> tradeRouteError);
+                if(tradeRouteErrorAlreadyRegistered)
+                {
+                    bool errorTown1 = true;
+                    bool errorTown2 = true;
+                    int num = 0;
+                    while(errorTown1 && errorTown2)
+                    {
+                        errorTown1 = tradeRouteError.TryGetValue(num, out Town townError1);
+                        errorTown2 = tempTownList.TryGetValue(num, out Town townError2);
+                        num++;
+                    }
+                    if (num < tradeRouteError.Count || num < tempTownList.Count)
+                    {
+                        InformationManager.DisplayMessage(new InformationMessage("While loading the trade route " + tradeRouteCount.Attribute("name").Value + ", a non-identical trade route was found. It one got replaced with the new loaded one."));
+                    }
+                    else
+                    {
+                        InformationManager.DisplayMessage(new InformationMessage("While loading the trade route " + tradeRouteCount.Attribute("name").Value + ", an indentical trade route was found. It got replaced with the new loaded one."));
+                    }
+                    tradeRoutes.Remove(tradeRouteCount.Attribute("name").Value);
+                }
+
                 tradeRoutes.Add(tradeRouteCount.Attribute("name").Value, new Dictionary<int, Town>(tempTownList));
                 tradeRouteList.Add(tradeRouteCount.Attribute("name").Value);
                 tempTownList.Clear();
